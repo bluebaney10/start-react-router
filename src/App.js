89eac798ac6c9d7bdbea37bcc42aa51137c18776
blue_baney <bluebaney10@gmail.com>
2018-12-31 19:50:59 +0700

    first react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import Routing from './routes'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="my-app">
+        <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="https://devahoy.com">
+                <img src={process.env.PUBLIC_URL + '/images/devahoy-text-logo.png'} alt="DEVAHOY LOGO" width="112" height="28" />
+              </a>
+            </div>
+            <div className="navbar-menu">
+              <div className="navbar-end">
+                <NavLink exact to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
+                <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
+                <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
+                <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
+                <a className="navbar-item" href="https://github.com/phonbopit" target="_blank">Star on <i className="fab fa-github"></i></a>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routing />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
